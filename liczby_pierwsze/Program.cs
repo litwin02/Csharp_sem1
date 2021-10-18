ebeaedb7fe36f3c66ddb7b2ee201c3e8dd3b1ab9
litwin02 <jakub.litwin02@wp.pl>
2021-10-18 21:37:42 +0200

    Niedkończone zadanie domowe

---
+++ liczby_pierwsze/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace liczby_pierwsze
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Podaj zakres wypisywania liczb pierwszych: ");
+            int zakres = int.Parse(Console.ReadLine());
+            int pierwiastek = (Int32)Math.Floor(Math.Sqrt(zakres));
+            
+
+            for(int i = 2; i <= zakres; i++)
+            {
+                for(int j = 2; j <= pierwiastek; j++)
+                {
+                    
+                }
+            }
+        }
+    }
+}
